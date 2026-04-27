5dc7d2babccaa72f8889059f0e6116a59124b34d
muhammet sumen <sumenmuhammet5@gmail.com>
2026-04-27 18:01:46 +0300

    chore: initial project setup with Vite + react + TS

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App(){
+  return(
+    <div>
+     <h1>Web Tasarımı ve Programlama</h1>
+     <h2>LAB-1</h2>
+     <p>Ad Soyad: [Muhammet Sümen]</p>
+     <p>Ogrenci No: [230 542 028]</p>
+    </div>
+  )
+}
+
+export default App
